773de22be2f120b759dde4febcd6eb60eb8452f1
zzent <zzent@live.com.mx>
2015-04-23 12:39:14 -0500

    05 se habilita la funcion de post de forma  automatica

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -5,20 +5,35 @@ modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
-		$scope.post = 
-		[{title: "post 1", upvotes: 5},
-		{title: "post 2", upvotes: 15},
-		{title: "post 3", upvotes: 7},
-		{title: "post 4", upvotes: 9},
-		{title: "post 5", upvotes: 3},
-		{title: "post 4", upvotes: 5}];
+		//Modelo
+		$scope.posts = 
+		[{title: "Post 1", upvotes: 5},
+		{title: "Post 2", upvotes: 15},
+		{title: "Post 3", upvotes: 7},
+		{title: "Post 4", upvotes: 9},
+		{title: "Post 5", upvotes: 3},
+		{title: "Post 6", upvotes: 5}];
 
-		//metodo de controlador
-		$scope.addPost = function(){
-			$scope.post.push(
-				{title:"Nuevo Post",
-				upvotes: 0});
-
+		//Metodo del controlador
+		$scope.addPost = function (){
+			if(!$scope.title || $scope.title === "")
+			{
+				alert("No se pertite postear titulos vacios");
+				return;
+			}
+			$scope.posts.push(
+				{
+					title: $scope.title,
+					link: $scope.link,
+					upvotes:0
+				});
+			//Two-way data binding
+			$scope.title = "";
+			$scope.link = "";
 		};
-
+		//Metodo para incrementar el voto 
+		$scope.incrementUpvotes = function (post)
+		{
+			post.upvotes += 1;
+		};
 	}]);
